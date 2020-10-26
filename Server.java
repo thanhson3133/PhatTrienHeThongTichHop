ee9aed4c1064fbf444e48ac368dad2769cd94323
thanhson3133 <72431446+thanhson3133@users.noreply.github.com>
2020-10-26 20:17:11 +0700

    Ứng dụng chat giữa Client và Server

--- Server.java
+++ Server.java
@@ -1,29 +1,46 @@
-package Socket;
+package lab4_UngDungChat;
 
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+
 public class Server {
+	private static Scanner scan;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		try {
-			ServerSocket server = new ServerSocket(9540);
+			@SuppressWarnings("resource")
+			ServerSocket server = new ServerSocket(8888);
 			System.out.println("Server da duoc tao");
 			Socket client = server.accept();
 			System.out.println("Client da ket noi den server");
-			Scanner inFromClient = new Scanner(client.getInputStream());
 			PrintStream outToClient = new PrintStream(client.getOutputStream());
-			outToClient.println("Xin chao, ban ten gi?");
-			String ten = inFromClient.nextLine();
-			System.out.println("clien: " + ten);
-			outToClient.println("Rat vui duoc tro chuyen voi " + ten);
+			outToClient.println("Xin Chao! Ban Muon Lam Gi?");
+			while(true) {
+				String obj = "exit";
+				
+				Scanner inFromClient = new Scanner(client.getInputStream());
+				outToClient = new PrintStream(client.getOutputStream());
+				String text = inFromClient.nextLine();
+				System.out.println("client: " + text);
+				if(text.equals(obj)) {
+					break;
+				}
+				System.out.print(">>>");
+				scan = new Scanner(System.in);
+				String txt = scan.nextLine();
+				outToClient.println(txt);
+				if(txt.equals(obj)) {
+					break;
+				}
+			}
+			outToClient.println("Close");
+			System.out.println("Close");
 			} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			}
-	}
-
+			}
 }
