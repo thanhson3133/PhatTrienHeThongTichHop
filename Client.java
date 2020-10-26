10a89890e5d8e372f61b1f60869ccd25b3fe21f7
thanhson3133 <72431446+thanhson3133@users.noreply.github.com>
2020-10-26 20:23:32 +0700

    Add files via upload

--- Client.java
+++ Client.java
@@ -1,4 +1,4 @@
-package lab4_UngDungChat;
+package lab4_DocSo;
 
 import java.io.IOException;
 import java.io.PrintStream;
@@ -7,33 +7,20 @@ import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Client {
-	private static Scanner inFromServer;
-	private static Scanner scan;
-
 	public static void main(String[] args) {
 		try {
 		@SuppressWarnings("resource")
-		Socket client = new Socket("LocalHost", 8888);
+		Socket client = new Socket("LocalHost", 7979);
 		System.out.println("Client da duoc tao");
-		inFromServer = new Scanner(client.getInputStream());
+		Scanner inFromServer = new Scanner(client.getInputStream());
 		PrintStream outToServer = new PrintStream(client.getOutputStream());
 		System.out.println("server: " + inFromServer.nextLine());
-		while(true) {
-			System.out.print(">>>");
-			scan = new Scanner(System.in);
-			String text = scan.nextLine();
-			outToServer.println(text);
-			String txt = inFromServer.nextLine();
-			System.out.println("server: " + txt);
-			if(text.equals("exit")) {
-				break;
-			}
-			if(txt.equals("exit")) {
-				System.out.println("server: " + inFromServer.nextLine());
-				break;
-			}		
-			}
-		
+		System.out.print(">>> ");
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		String ten = scan.nextLine();
+		outToServer.println(ten);
+		System.out.println("server: " + inFromServer.nextLine());
 		} catch (UnknownHostException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
